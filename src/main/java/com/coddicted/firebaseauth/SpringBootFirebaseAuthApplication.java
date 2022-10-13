77ca5ff58e92f10bae0c61b96bfcb3fc137cba33
payamrastogi <payamrastogi@outlook.com>
2022-10-12 22:46:44 -0400

    Initial commit

---
+++ src/main/java/com/coddicted/firebaseauth/SpringBootFirebaseAuthApplication.java
@@ -0,0 +1,13 @@
+package com.coddicted.firebaseauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFirebaseAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootFirebaseAuthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/coddicted/firebaseauth/SpringBootFirebaseAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coddicted.firebaseauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootFirebaseAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
